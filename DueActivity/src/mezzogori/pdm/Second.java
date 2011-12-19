c99f6b487a334a63cc8a452749d902bb2d60ab9b
mezzogorimirko <genji_90@hotmail.it>
2011-12-19 17:21:27 +0100

    DueActivity

---
+++ DueActivity/src/mezzogori/pdm/Second.java
@@ -0,0 +1,13 @@
+package mezzogori.pdm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+    }
+}
